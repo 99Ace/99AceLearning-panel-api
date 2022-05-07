998b68be6c3abb94cd4fbd487a1f5deb88c0ff9b
99Ace <ace.ang@trentglobal.edu.sg>
2022-05-07 11:37:22 +0000

    Created hbs for auth routes

--- routes/landing.js
+++ routes/landing.js
@@ -4,7 +4,7 @@ const router = express.Router(); // #1 - Create a new express Router
 
 //  #2 Add a new route to the Express router
 router.get('/', (req,res)=>{
-    res.send("Hello Ace Learning world Landing")
+    res.render('index.hbs')
 })
 
 module.exports = router; // #3 export out the router

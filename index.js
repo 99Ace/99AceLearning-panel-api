23268e1433c125bd21d24f47b3688465719ba0a0
99Ace <chunliang.ang@gmail.com>
2022-05-09 15:10:30 +0800

    set up bookshelf, models, forms and successfully register a user using the form

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
 
+// Set up Sessions
+const session = require('express-session');
+const flash = require('connect-flash');
+const FileStore = require('session-file-store')(session);
+
 require("dotenv").config();
 
 // create an instance of express app
@@ -24,9 +29,25 @@ app.use(
   })
 );
 
+// set up sessions
+app.use(session({
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 // Import in the routes
 const landingRoutes = require('./routes/landing');
 const authRoutes = require('./routes/auth');
+
+app.use(flash())
+// Register Flash middleware
+app.use(function (req, res, next) {
+    res.locals.success_messages = req.flash("success_messages");
+    res.locals.error_messages = req.flash("error_messages");
+    next();
+});
 
 async function main() {
     
@@ -36,7 +57,7 @@ async function main() {
 
     
     
-    app.listen(process.env.PORT||3000, () => {
+    app.listen(3000, () => {
         console.log("Server has started");
       });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const router = express.Router(); // #1 - Create a new express Router
 
+// import in the User model
+const { User } = require('../models');
+// import in the forms
+const { createRegistrationForm, bootstrapField } = require('../forms');
+
+
 
 //  #2 Add a new route to the Express router
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.send("Profile")
 })
-router.get('/login', (req,res)=>{
-    res.send("Login")
+router.get('/login', (req, res) => {
+    res.render("auth/login")
 })
-router.get('/logout', (req,res)=>{
+router.get('/logout', (req, res) => {
     res.send("Logout")
 })
-router.get('/register', (req,res)=>{
-    res.send("Register")
+
+// Register Routes
+router.get('/register', (req, res) => {
+    // display the registration form
+    const registerForm = createRegistrationForm();
+    res.render('auth/register.hbs', {
+        'form': registerForm.toHTML(bootstrapField)
+    })
+})
+router.post('/register', (req, res) => {
+    const registerForm = createRegistrationForm();
+    registerForm.handle(req, {
+        success: async (form) => {
+            const user = new User({
+                'username': form.data.username,
+                'password': form.data.password,
+                'email': form.data.email
+            });
+            await user.save();
+            req.flash("success_messages", "User signed up successfully!");
+            res.redirect('/auth/login')
+        },
+        'error': (form) => {
+            res.render('users/register', {
+                'form': form.toHTML(bootstrapField)
+            })
+        }
+    })
 })
 
 module.exports = router; // #3 export out the router

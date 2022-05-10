782a9245b2d621cd71292cb1353b4e3a7929692f
99Ace <ace.ang@trentglobal.edu.sg>
2022-05-10 05:41:17 +0000

    Added Postgres

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,12 +1,16 @@
 // Setting up the database connection
 const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      user: 'mikar',
-      password:'M988m988!',
-      database:'acelearning'
+  'client': process.env.DB_DRIVER,
+  'connection': {
+    'user': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_DATABASE,
+    'host': process.env.DB_HOST,
+    'ssl': {
+      'rejectUnauthorized': false
     }
-  })
-  const bookshelf = require('bookshelf')(knex)
-  
-  module.exports = bookshelf;
+  }
+})
+const bookshelf = require('bookshelf')(knex)
+
+module.exports = bookshelf;
